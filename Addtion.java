5b71a83e0d998cbf0d3d9805da6b227004da8ad2
DiwateManasi <themanasidiwate@gmail.com>
2023-12-19 09:06:30 +0530

    added d in addition

--- Addtion.java
+++ Addtion.java
@@ -1,11 +1,11 @@
 class Addition
 {
-    public static int add(int a, int b, int c)
+    public static int add(int a, int b, int c, int d)
     {
-        return a+b+c;
+        return a+b+c+d;
     }
     public static void main(String [] args)
     {
-        System.out.println(add(2,4,5));
+        System.out.println(add(2,4,5,7));
     }
 }
