1b96669baffec2a4f481478782d1ef4d42c2bb03
DiwateManasi <154223721+DiwateManasi@users.noreply.github.com>
2023-12-19 08:56:50 +0530

    Update Addtion.java
    added one more variable

--- Addtion.java
+++ Addtion.java
@@ -1,11 +1,11 @@
 class Addition
 {
-    public static int add(int a, int b)
+    public static int add(int a, int b, int c)
     {
-        return a+b;
+        return a+b+c;
     }
     public static void main(String [] args)
     {
-        System.out.println(add(2,4));
+        System.out.println(add(2,4,5));
     }
 }
